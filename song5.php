0c13496e77710fed88725e6567a7ca587ad05cba
MartillanoBillFrancis <francisfrancisbill@gmail.com>
2022-04-16 17:00:03 +0800

    song 5 final

---
+++ song5.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Lyrics Sanctuary</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                   
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Lyrics Sanctuary</h1>
+                <p class="lead">Where words fail, music speaks</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        
+                        <?php
+                         echo "<h2 class='text-primary'>You</h2>";
+                         echo "<p class='lead '>Basil Valdez</p>";
+
+                         echo "<p>You pushed me up</p>";
+                         echo "<p>When I'm about to give up</p>";
+                         echo "<p>You're on my side when no one seems to listen</p>";
+                         echo "<p>And if you go</p>";
+                         echo "<p>You know the tears can't help but show</p>";
+                         echo "<p>You break this heart and tear it apart</p>";
+                         echo "<p>Then suddenly the madness starts</p>";
+                         echo "<br/>";
+
+                         echo "<p>It's your smile</p>";
+                         echo "<p>Your face, your lips that I miss</p>";
+                         echo "<p>Those sweet little eyes that stare at me and make me say</p>";
+                         echo "<p>I'm with you through all the way 'cause it's you</p>";
+                         echo "<p>Who fills the emptiness in me</p>";
+                         echo "<p>It changes everything, you see</p>";
+                         echo "<p>When I know I've got you with me</p>";
+                         echo "<br/>";
+
+                         echo "<p>It's your smile</p>";
+                         echo "<p>Your face, your lips that I miss</p>";
+                         echo "<p>Those sweet little eyes that stare at me and make me say</p>";
+                         echo "<p>I'm with you through all the way 'cause it's you</p>";
+                         echo "<p>Who fills the emptiness in me</p>";
+                         echo "<p>It changes everything, you see</p>";
+                         echo "<p>When I know I've got you with me</p>";
+                         echo "<br/>";
+
+                         echo "<p>You pushed me up</p>";
+                         echo "<p>When I'm about to give up</p>";
+                         echo "<p>You're on my side when no one seems to listen</p>";
+                         echo "<p>And if you go</p>";
+                         echo "<p>You know the tears can't help but show</p>";
+                         echo "<p>You break this heart and tear it apart</p>";
+                         echo "<p>Then suddenly the madness starts</p>";
+                         echo "<br/>";
+
+                         echo "<p>It's your smile</p>";
+                         echo "<p>Your face, your lips that I miss</p>";
+                         echo "<p>Those sweet little eyes that stare at me and make me say</p>";
+                         echo "<p>I'm with you through all the way 'cause it's you</p>";
+                         echo "<p>Who fills the emptiness in me</p>";
+                         echo "<p>It changes everything, you see</p>";
+                         echo "<p>When I know I've got you with me</p>";
+                         echo "<br/>";
+
+                         echo "<p>It's your smile</p>";
+                         echo "<p>Your face, your lips that I miss</p>";
+                         echo "<p>Those sweet little eyes that stare at me and make me say</p>";
+                         echo "<p>I'm with you through all the way 'cause it's you</p>";
+                         echo "<p>Who fills the emptiness in me</p>";
+                         echo "<p>It changes everything, you see</p>";
+                         echo "<p>When I know I've got you with me</p>";
+                         echo "<br/>";
+                         
+
+                        ?>
+                        Back to main page <a href = "index.php"> here!</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Bill Francis L. Martillano</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
